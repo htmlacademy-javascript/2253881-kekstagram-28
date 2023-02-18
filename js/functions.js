caafd26fa01127ec848286c58781fcbe22fa5a4f
xMurzik <murzik6667@gmail.com>
2023-02-18 14:12:34 +0300

    added padStart function

--- js/functions.js
+++ js/functions.js
@@ -25,6 +25,29 @@ function pullingNumber(string) {
     : `Результат: число ${Number(string.replace(/\D/g, ''))}`;
 }
 
+function stringEditor(startString, maxLength, addedString) {
+  if (startString.length >= maxLength) {
+    return `Результат: строка ${startString}`;
+  }
+
+  if (addedString.length >= maxLength) {
+    const arrayedAddedString = addedString.split('');
+
+    while (arrayedAddedString.length + startString.length !== maxLength) {
+      arrayedAddedString.pop();
+    }
+    arrayedAddedString.push(startString);
+    return `Результат: строка '${arrayedAddedString.join('')}'`;
+  }
+
+  maxLength = maxLength - startString.length;
+  if (maxLength > addedString.length) {
+    addedString += addedString.repeat(maxLength / addedString.length);
+  }
+  return addedString.slice(0, maxLength) + String(startString);
+}
+
 lengthStringChecker('asdasdasdadadadasd', 10);
 palindromChecker('Лёша на полке клопа нашёл ');
 pullingNumber('adjskdahsdk ajsdkhasdkjhs.../ // //');
+stringEditor('q', 4, 'we');
