a2bd0d7a597e3a1eb52f8740853f63f197ea4d7a
xMurzik <murzik6667@gmail.com>
2023-03-12 16:15:51 +0300

    deleted queryselectors

--- js/main.js
+++ js/main.js
@@ -3,6 +3,3 @@
 import renderPicksIntoWindow from './render-picks.js';
 
 renderPicksIntoWindow();
-
-// document.querySelector('.social__comment-count').classList.add('hidden');
-// document.querySelector('.comments-loader').classList.add('hidden');
